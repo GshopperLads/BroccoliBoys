b6d7e85349a3f12fdca64715f37fb781c6bf5ead
Alan Yoho <alanyoh@gmail.com>
2018-07-25 18:18:41 -0400

    fixed my bug, sorry guys

--- client/components/ProductCreator.js
+++ client/components/ProductCreator.js
@@ -1,14 +1,15 @@
+import React from "react"
 const ProductCreator = (props) => {
     const products = [...props.products]
     return (
         <ul>
-            products.map(product => {
+            {products.map(product =>
                 <li key={product.id}>
                     {' '}
                     {product.name} {product.imageUrl} {product.price}{' '}
                     {product.quantity}{' '}
                 </li>
-            })
+            )}
         </ul>
 
     )
