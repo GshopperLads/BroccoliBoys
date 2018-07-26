a7f83959f54c0d21ae5c0455de099d0877515bc4
Alan Yoho <alanyoh@gmail.com>
2018-07-26 11:09:16 -0400

    syntax check

--- client/components/ProductCreator.js
+++ client/components/ProductCreator.js
@@ -1,17 +1,13 @@
 import React from "react"
+
 const ProductCreator = (props) => {
     const products = [...props.products]
     return (
         <ul>
             {products.map(product =>
-                <li key={product.id}>
-                    {' '}
-                    {product.name} {product.imageUrl} {product.price}{' '}
-                    {product.quantity}{' '}
-                </li>
+                <li key={product.id}>{' '}{product.name}{product.imageUrl}{product.price}{' '}{product.quantity}{' '}</li>
             )}
         </ul>
-
     )
 }
 
