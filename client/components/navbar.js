3ab02502202fc516a05dda3d010d1e92c78c3fec
Synuhe <synuhe.uribe@gmail.com>
2018-07-27 13:09:16 +0100

    first stab at homepage

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -27,14 +27,14 @@ const Navbar = ({ handleClick, isLoggedIn }) => (
   </a>
           </Link>
         </div>
-        <div className="item">
+        {/* <div className="item">
 
           <div className="ui transparent icon input">
             <input type="text" placeholder="Search..." />
             <i className="search link icon"></i>
           </div>
 
-        </div>
+        </div> */}
       </div>
     </div>
     <div className="ui segment">
