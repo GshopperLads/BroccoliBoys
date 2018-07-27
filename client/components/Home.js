05cfed970c05046264027bf6e847bc5915f2544b
Alan Yoho <alanyoh@gmail.com>
2018-07-27 16:35:32 -0400

    updated search feature

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,10 +1,44 @@
 import React from "react"
-import Carousel from "./Carousel"
-import { Container } from "../../node_modules/semantic-ui-react";
+
 
 const HomePage = () => {
     return (
-        <Carousel />
+        <React.Fragment>
+            <script type="text/javascript">
+                $(document).ready(function(){
+                    window.setInterval(function () {
+                        var next = ($('.select').index() + 1) % $('.circle').length;
+                        var prev = $('.select').index();
+
+                        $('.select').removeClass('select');
+                        $('.circle').eq(next).addClass('select');
+
+
+                        $('.slide').eq(next).addClass('open').css('left', '100%');
+                        $('.slide').eq(prev).animate({ 'left': '-100%' }, 700);
+                        $('.slide').eq(next).animate({ 'left': '0%' }, 700);
+                    }, 7000)})
+        </script>
+
+            <div className="slidelist">
+                <div className="slide open" style={{ left: "-100%" }}><span className="image1"></span></div>
+                <div className="slide open" style={{ left: "-100%" }}><span className="image2"></span></div>
+                <div className="slide open" style={{ left: "-100%" }}><span className="image3"></span></div>
+                <div className="slide open" style={{ left: "-100%" }}><span className="image4"></span></div>
+                <div className="slide open" style={{ left: "0%" }}><span className="image5"></span></div>
+
+                <div className="bottomButton">
+                    <ul>
+                        <li className="circle"><i className="fa fa-circle-o"></i></li>
+                        <li className="circle"><i className="fa fa-circle-o"></i></li>
+                        <li className="circle"><i className="fa fa-circle-o"></i></li>
+                        <li className="circle"><i className="fa fa-circle-o"></i></li>
+                        <li className="circle select"><i className="fa fa-circle-o"></i></li>
+                    </ul>
+                </div>
+            </div>
+        </React.Fragment>
     )
+
 }
 export default HomePage
