1070c3a2e20e1101d4c97e923a308960dade11fd
Alan Yoho <alanyoh@gmail.com>
2018-07-31 17:58:43 -0400

    stuff

--- client/components/Card.js
+++ client/components/Card.js
@@ -6,87 +6,84 @@ import { connect } from 'react-redux'
 
 
 class CardExampleCardProps extends React.Component {
-
-
-
   render() {
     return (
       <React.Fragment>
-      {this.props.isLoggedIn ? (
-        <div>
-        <Card
-          image={this.props.image}
-          header={this.props.name}
-          meta={`$${this.props.price}/head | (${
-            this.props.quantity
-            }) in stock.`}
+        {this.props.isLoggedIn ? (
+          <div>
+            <Card
+              image={this.props.image}
+              header={this.props.name}
+              meta={`$${this.props.price}/head | (${
+                this.props.quantity
+                }) in stock.`}
 
-          description={this.props.description}
-          extra={
-            <div>
-              <div
-                className="ui vertical animated button"
-                tabIndex="0"
-                onClick={async() =>{
-                  await this.props.Shop(this.props.product.id, this.props.userId)
-                  await this.props.fetchCartFromDb(this.props.userId)
-                }
-                }
-              >
-                <div className="hidden content">Add</div>
-                <div className="visible content">
-                  <i className="shop icon" />
-                </div>
-              </div>
-              {!this.props.isSingle &&
-                <Link to={`/products/${this.props.product.id}`}>
-                  <button className="ui button">See More</button>
-                </Link>
-              }
-              <Icon name="recycle" />
-              Certified Organic
+              description={this.props.description}
+              extra={
+                <div>
+                  <div
+                    className="ui vertical animated button"
+                    tabIndex="0"
+                    onClick={async () => {
+                      await this.props.Shop(this.props.product.id, this.props.userId)
+                      await this.props.fetchCartFromDb(this.props.userId)
+                    }
+                    }
+                  >
+                    <div className="hidden content">Add</div>
+                    <div className="visible content">
+                      <i className="shop icon" />
+                    </div>
+                  </div>
+                  {!this.props.isSingle &&
+                    <Link to={`/products/${this.props.product.id}`}>
+                      <button className="ui button">See More</button>
+                    </Link>
+                  }
+                  <Icon name="recycle" />
+                  Certified Organic
             </div>
-          }
-        />
-      </div>
-      ) : (
-        <div>
-        <Card
-          image={this.props.image}
-          header={this.props.name}
-          meta={`$${this.props.price}/head | (${
-            this.props.quantity
-            }) in stock.`}
-
-          description={this.props.description}
-          extra={
-            <div>
-              <div
-                className="ui vertical animated button"
-                tabIndex="0"
-                onClick={() =>
-                  sessionStorage.setItem([this.props.product.id], 1)
-
-                }
-              >
-                <div className="hidden content">Add</div>
-                <div className="visible content">
-                  <i className="shop icon" />
-                </div>
-              </div>
-              {!this.props.isSingle &&
-                <Link to={`/products/${this.props.product.id}`}>
-                  <button className="ui button">See More</button>
-                </Link>
               }
-              <Icon name="recycle" />
-              Certified Organic
-            </div>
-          }
-        />
-      </div>
+            />
+          </div>
+        ) : (
+            <div>
+              <Card
+                image={this.props.image}
+                header={this.props.name}
+                meta={`$${this.props.price}/head | (${
+                  this.props.quantity
+                  }) in stock.`}
 
-      )}
+                description={this.props.description}
+                extra={
+                  <div>
+                    <div
+                      className="ui vertical animated button"
+                      tabIndex="0"
+                      onClick={() =>
+                        sessionStorage.setItem([this.props.product.id], 1)
+
+                      }
+                    >
+                      <div className="hidden content">Add</div>
+                      <div className="visible content">
+                        <i className="shop icon" />
+                      </div>
+                    </div>
+                    {!this.props.isSingle &&
+                      <Link to={`/products/${this.props.product.id}`}>
+                        <button className="ui button">See More</button>
+                      </Link>
+                    }
+                    <Icon name="recycle" />
+                    Certified Organic
+            </div>
+                }
+              />
+            </div>
+
+          )}
       </React.Fragment>
     )
   }
