21352236c1d174770463b2dae6bd06ee6ebdf548
J Song <feelpnw@gmail.com>
2018-08-08 15:21:55 -0400

    added

--- client/components/Allproducts.js
+++ client/components/Allproducts.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { fetchProducts, Shop, fetchCart } from '../store'
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {fetchProducts, Shop, fetchCart} from '../store'
 
 import ProductCreator from './ProductCreator'
 
@@ -16,78 +16,77 @@ export class AllProducts extends Component {
       await this.props.fetchProducts()
     }
 
-    this.setState({ products: this.props.products })
+    this.setState({products: this.props.products})
 
     if (typeof document !== 'undefined') {
+      var el = document.getElementById('searchBar2')
 
-    var el = document.getElementById('searchBar2');
-
-    if (el) {
-      el.addEventListener(
-        'input',
-        function (evt) {
-
-          const products = this.props.products
-          let vals = products.filter(product =>
-            product.name.toLowerCase().includes(evt.target.value.toLowerCase())
-          )
-          this.setState({ products: vals })
-
-        }.bind(this)
-      )
+      if (el) {
+        el.addEventListener(
+          'input',
+          function(evt) {
+            const products = this.props.products
+            let vals = products.filter(product =>
+              product.name
+                .toLowerCase()
+                .includes(evt.target.value.toLowerCase())
+            )
+            this.setState({products: vals})
+          }.bind(this)
+        )
+      }
+      var el = document.getElementById('searchBar1')
+      if (el) {
+        el.addEventListener(
+          'input',
+          function(evt) {
+            const products = this.props.products
+            let vals = products.filter(product =>
+              product.name
+                .toLowerCase()
+                .includes(evt.target.value.toLowerCase())
+            )
+            this.setState({products: vals})
+          }.bind(this)
+        )
+      }
     }
-    var el = document.getElementById('searchBar1')
-    if (el) {
-      el.addEventListener(
-        'input',
-        function (evt) {
-          const products = this.props.products
-          let vals = products.filter(product =>
-            product.name.toLowerCase().includes(evt.target.value.toLowerCase())
-          )
-          this.setState({ products: vals })
-
-        }.bind(this)
-      )
-    }
-  }
   }
   componentWillUnmount() {
     if (typeof document !== 'undefined') {
-
-    var el = document.getElementById('searchBar2')
-    if (el) {
-      document.removeEventListener(
-        'input',
-        function (evt) {
-
-          const products = this.props.products
-          let vals = products.filter(product =>
-            product.name.toLowerCase().includes(evt.target.value.toLowerCase())
-          )
-          this.setState({ products: vals })
-
-        }.bind(this)
-      )
+      var el = document.getElementById('searchBar2')
+      if (el) {
+        document.removeEventListener(
+          'input',
+          function(evt) {
+            const products = this.props.products
+            let vals = products.filter(product =>
+              product.name
+                .toLowerCase()
+                .includes(evt.target.value.toLowerCase())
+            )
+            this.setState({products: vals})
+          }.bind(this)
+        )
+      }
+      var el = document.getElementById('searchBar1')
+      if (el) {
+        document.removeEventListener(
+          'input',
+          function(evt) {
+            const products = this.props.products
+            let vals = products.filter(product =>
+              product.name
+                .toLowerCase()
+                .includes(evt.target.value.toLowerCase())
+            )
+            this.setState({products: vals})
+          }.bind(this)
+        )
+      }
     }
-    var el = document.getElementById('searchBar1')
-    if (el) {
-      document.removeEventListener(
-        'input',
-        function (evt) {
 
-          const products = this.props.products
-          let vals = products.filter(product =>
-            product.name.toLowerCase().includes(evt.target.value.toLowerCase())
-          )
-          this.setState({ products: vals })
-
-        }.bind(this)
-      )
-    }
-  }
-
-    $(function () {
+    $(function() {
       $('#example').barrating({
         theme: 'fontawesome-stars-o'
       })
